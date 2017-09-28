0bdcfccdf09000c9d64a034b802e2cf7325ace0c
dmitriyvelichko <dmitriy.velichko.dev@gmail.com>
2017-09-28 23:32:17 +0300

    add test for GET organizations/:name/relations, resolve async bugs

--- tests/misc.test.js
+++ tests/misc.test.js
@@ -1,38 +1,38 @@
-const request = require('supertest-as-promised'),
-    httpStatus = require('http-status'),
-    chai = require('chai');
+const httpStatus = require('http-status'),
+    chai = require('chai'),
+    chaiHttp = require('chai-http');
 
 const app = require('../app'),
     expect = chai.expect;
 
 chai.config.includeStack = true;
+chai.use(chaiHttp);
 
 
 describe('## Misc', () => {
 
     describe('# GET /api/service-check', () => {
         it('should return OK', (done) => {
-            request(app)
+            chai.request(app)
                 .get('/api/service-check')
-                .expect(httpStatus.OK)
-                .then((res) => {
+                .end((err, res) => {
+                    expect(res.status).to.equal(httpStatus.OK);
                     expect(res.body.message).to.equal('OK');
                     done();
                 })
-                .catch(done);
+
         });
     });
 
     describe('# GET /api/404', () => {
         it('should return 404 status', (done) => {
-            request(app)
+            chai.request(app)
                 .get('/api/404')
-                .expect(httpStatus.NOT_FOUND)
-                .then((res) => {
+                .end((err, res) => {
+                    expect(res.status).to.equal(httpStatus.NOT_FOUND);
                     expect(res.body.message).to.equal('API not found');
                     done();
                 })
-                .catch(done);
         });
     });
 });
--- tests/organizations.test.js
+++ tests/organizations.test.js
@@ -1,6 +1,6 @@
-const request = require('supertest-as-promised'),
-    httpStatus = require('http-status'),
-    chai = require('chai');
+const httpStatus = require('http-status'),
+    chai = require('chai'),
+    chaiHttp = require('chai-http');
 
 const app = require('../app'),
     dbInit = require('../config/dbInit'),
@@ -8,6 +8,8 @@ const app = require('../app'),
     expect = chai.expect;
 
 chai.config.includeStack = true;
+chai.use(chaiHttp);
+
 
 /*
  Use careful only in testing
@@ -20,32 +22,33 @@ before(function(done) {
     dbInit.creatingConnection()
         .then(dbInit.creatingDatabase)
         .then(dbCleanUp)
-        .then(() => done())
+        .then(() => {
+            done();
+        })
     }
 );
 
 describe('## Testing /api/organizations', function() {
 
     describe('# POST /api/organizations', function() {
-        beforeEach(function(done) {
-            dbCleanUp()
-                .then(() => done())
+        beforeEach((done) => {
+            dbCleanUp().then(() => {
+                done();
+            });
         });
-
         let orgsSuccess = {
                 "org_name": "Paradise Island",
                 "daughters": [{"org_name":"Banana tree"}]
             };
         it('should create organizations with their relations', function(done) {
-            request(app)
+            chai.request(app)
                 .post('/api/organizations')
                 .send(orgsSuccess)
-                .expect(httpStatus.CREATED)
-                .then((res) => {
-                    expect(res.body.message).to.equal(httpStatus[201]);
+                .end((err, res) => {
+                    expect(res.status).to.equal(httpStatus.CREATED);
+                    expect(res.body.message).to.equal("Created");
                     done();
-                })
-                .catch(() => done())
+                });
         });
 
         let orgsFailedInvalidFormat = {
@@ -53,15 +56,14 @@ describe('## Testing /api/organizations', function() {
             "daughters": {"org_name":"Banana tree"}
         };
         it('should fails due invalid format post data', function(done) {
-            request(app)
+            chai.request(app)
                 .post('/api/organizations')
                 .send(orgsFailedInvalidFormat)
-                .expect(httpStatus.INTERNAL_SERVER_ERROR)
-                .then((res) => {
+                .end((err, res) => {
+                    expect(res.status).to.equal(httpStatus.INTERNAL_SERVER_ERROR);
                     expect(res.body.message).to.equal(httpStatus[500]);
                     done();
-                })
-                .catch(() => done())
+                });
         });
 
         let orgsFailedReferenceToItself = {
@@ -69,27 +71,61 @@ describe('## Testing /api/organizations', function() {
             "daughters": [{"org_name":"Paradise Island"}]
         };
         it('should fails due org references to itself', function(done) {
-            request(app)
+            chai.request(app)
                 .post('/api/organizations')
                 .send(orgsFailedReferenceToItself)
-                .expect(httpStatus.UNPROCESSABLE_ENTITY)
-                .then((res) => {
+                .end((err, res) => {
+                    expect(res.status).to.equal(httpStatus.UNPROCESSABLE_ENTITY);
                     expect(res.body.message).to.equal(
                         `Received data has organization that referenced to itself: ${orgsFailedReferenceToItself.org_name}`);
                     done();
-                })
-                .catch(() => done())
+                });
         });
-
-
-
-
     });
 
-    // describe('# GET /api/organizations', function() {
-    //
-    //     beforeEach(function() {
-    //         return dbCleanUp;
-    //     });
+    describe('# GET /api/organizations', function() {
+        describe("Test for get hierarchy", function() {
+            before(function (done) {
+                dbCleanUp()
+                    .then(() => {
+                        return models.Organization.bulkCreate([
+                                {name: "org1Level1"},
+                                {name: "org1Level2"},
+                                {name: "org2Level2"},
+                                {name: "org1Level3"}
+                            ])
+                    })
+                    .then(() => {
+                        return models.Relation.bulkCreate([
+                            {id: 1, parentName: "org1Level1", daughterName: "org1Level2"},
+                            {id: 2, parentName: "org1Level1", daughterName: "org2Level2"},
+                            {id: 3, parentName: "org1Level2", daughterName: "org1Level3"},
+                            {id: 4, parentName: "org2Level2", daughterName: "org1Level3"}
+                        ])
+                    })
+                    .then(() => done())
+            });
+
+            it('should return right relations list', function(done) {
+                chai.request(app)
+                    .get('/api/organizations/org1Level2/relations')
+                    .end((err, res) => {
+                        expect(res.status).to.equal(httpStatus.OK);
+                        expect(res.body).to.deep.equal({
+                            page: 1,
+                            per_page: 100,
+                            page_count: 1,
+                            total_count: 3,
+                            records:
+                                [{ org_name: 'org1Level3', relationship_type: 'daughter' },
+                                    { org_name: 'org1Level1', relationship_type: 'parent' },
+                                    { org_name: 'org2Level2', relationship_type: 'sister' } ]
+                        });
+                        done();
+                    });
+            });
+        });
+
+    });
 
 });
